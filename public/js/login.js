4cade754b436086af97e0af31e22192592264382
Cole Hartkopp <chsoccer4@outlook.com>
2023-03-25 12:33:18 -0500

    login fetch

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,26 @@
 // login fetch API client JS
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // login values username and password correct?
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        // this is correct fetch path?
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // if login successful send to homepage?
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
