d972c922b233e82adad22eb36b2df2e108efacce
Cole Hartkopp <chsoccer4@outlook.com>
2023-03-25 12:36:25 -0500

    signup fetch

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,26 @@
 // signup fetch API client JS
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    // signup values username and password correct? was not sure to call it by username or just name
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        // this is correct fetch path?
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // if signup successful send to homepage?
+            document.location.replace('/homepage');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
