be84b18f38ce6d6e9d8455ccce7a53af6dee9b16
kitrath <crathmel@gmail.com>
2023-03-28 14:16:21 -0500

    add PUT method controller for madlibinstance

--- controllers/api/radLibRoutes.js
+++ controllers/api/radLibRoutes.js
@@ -6,22 +6,22 @@ const {
     UserInput,
 } = require('../../models');
 const withAuth = require('../../utils/auth');
-const { body, check, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-// Create a set of UserInput(s) and associate
-// them with a MadLibInstance (without a completed_text
-// after this route). If this request is successful, i.e.,
-// if a MadLibInstance and it's associated UserInput(s)
-// are successfully created, then the client JS will
-// redirect to '/radlibs/:id' (homeRoutes.js), where
-// the user will have the option to save the completed
-// text of the madlib as displayed in the handlebars template.
+// 1. Validate Input - Return status 422 if errors
+// 2. Create a MadLibInstance and associate it with its
+//  parent MadLibForm (MadLibForm ID retrieved from session)
+// 3. Create UserInputs from POST request body (req.body) and
+//  associate them with the just created, parent MadLibInstance
+// 4. Upon return response to client fetch() request, user will
+//  be redirected to /radlibs/:id page, where they can choose
+//  to save the completed content of the radlib.
 router.post(
     '/', 
-    withAuth,
+    //withAuth,
     body('inputs').isArray(),
-    check('inputs.*.content').isString().notEmpty(),
-    check('inputs.*.blank_id').notEmpty().isInt(),
+    body('inputs.*.content').not().isEmpty().trim().escape(),
+    body('inputs.*.blank_id').not().isEmpty().isInt(),
     async (req, res) => {
         const errors = validationResult(req);
 
@@ -67,10 +67,43 @@ router.post(
 // the user a save button.
 router.put(
     '/:id', 
-    withAuth,
-    body('completed_text').isString().notEmpty(),
+    //withAuth,
+    body('completed_text').not().isEmpty().trim().escape(),
     async (req, res) => {
+        const errors = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+
+        try {
+            const instanceId =  req.params.id;
+            const madLibInstance = await MadLibInstance.update(
+                { completed_text: req.body.completed_text },
+                {
+                    where: {
+                        id: instanceId,
+                    },
+                }
+            );
+            
+            if (!madLibInstance) {
+                res.status(404).json({
+                    success: false,
+                    message: `radlib with id ${instanceId} not found.`
+                });
+                return;
+            }
+
+            res.status(200).json({
+                success: true,
+                message: `Successfully added text to radlib with id ${instanceId}.`
+            });
+        }  catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
 });
 
 module.exports = router;
